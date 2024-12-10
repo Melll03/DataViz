c3b661b3d8492e1d2caa426056a576f0c9974f90
Melll03 <melissa.cmr03@gmail.com>
2024-12-10 21:01:25 +0100

    ajout nb touristes pour la carte
    ajout nb de touristes pour la carte par région pour l'année 2015

--- map.js
+++ map.js
@@ -7,6 +7,36 @@ document.addEventListener('DOMContentLoaded', function () {
       KR45: 'North Jeolla', KR46: 'South Jeolla', KR47: 'North Gyeongsang',
       KR48: 'South Gyeongsang', KR49: 'Jeju', KR50: 'Sejong'
     };
+
+    const regionVisitors = {
+        "Seoul": 12451891,
+        "Busan": 7158553,
+        "Daegu": 3163161,
+        "Incheon": 4407063,
+        "Gwangju": 2135332,
+        "Daejeon": 2984929,
+        "Ulsan": 1632410,
+        "Sejong": 333329,
+        "Gyeonggi": 15451755,
+        "Gangwon": 11559005,
+        "North Chungcheong": 5141110,
+        "South Chungcheong": 9944616,
+        "North Jeolla": 6760830,
+        "South Jeolla": 8063538,
+        "North Gyeongsang": 8822201,
+        "South Gyeongsang": 8479567,
+        "Jeju": 4732494
+      };
+  
+      function formatVisitors(visitors) {
+          if (visitors >= 1000000) {
+              return (visitors / 1000000).toFixed(1) + 'M'; // Affiche en millions
+          } else if (visitors >= 1000) {
+              return (visitors / 1000).toFixed(1) + 'K'; // Affiche en milliers
+          } else {
+              return visitors; 
+          }
+      }
     
     fetch('south-korea.svg') // Charge le fichier SVG
       .then(response => response.text())
